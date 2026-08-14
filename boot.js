071711914c12b77dcc1b4a2fe092f1fb903a0684
iki1uc <entertrainer@web.de>
2026-08-14 15:46:47 +0200

    Add multiple module loading statements to boot.js

--- boot.js
+++ boot.js
@@ -1,1 +1,21 @@
+DOOR laden
 
+DOO laden
+
+RESPO laden
+
+OS laden
+
+NC.engine laden
+
+243 laden
+
+SCORE laden
+
+WETTE laden
+
+SLIDE laden
+
+alle Module exportieren
+
+start.js damit versorgen
