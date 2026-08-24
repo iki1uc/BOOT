b985b65c407b22ded3552da7de3ccd1b98a5efcb
iki1uc <entertrainer@web.de>
2026-08-24 20:25:47 +0200

    Update masterboot.js

--- masterboot.js
+++ masterboot.js
@@ -1,17 +1,25 @@
-# BOOT.geo
-zone: "BOOT"
-pos: [0,0,0]
-dir: "C"
-val: 0
-mov: "none"
+masterboot = {
 
-memory:
-  type: "ram-rom"
-  mode: "virtual"
-  location: "RAM"
-  bootable: true
+  load: async function(file) {
+    return await OS.net.read(file)
+  },
 
-rules:
-  - keine Drift
-  - keine Fremdlogik
-  - OS-only
+  apply: function(geo) {
+    OS.zone = geo.zone
+    OS.pos  = geo.pos
+    OS.dir  = geo.dir
+    OS.val  = geo.val
+    OS.mov  = geo.mov
+    OS.memory = geo.memory
+  },
+
+  start: async function() {
+
+    if (!OS.net.online()) {
+      throw "NET required: System cannot start offline."
+    }
+
+    let geo = await this.load("BOOT.geo")
+    this.apply(geo)
+  }
+}
